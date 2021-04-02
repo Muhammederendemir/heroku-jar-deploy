6976e05336a62a6e5655917ff67813387c4b1b60
Muhammederendemir <45883661+mhmmderen3@users.noreply.github.com>
2021-04-02 16:13:14 +0300

    add heroku example

--- src/main/java/com/metin/heroku/spring/boot/jar/deploy/herokuspringbootjardeploy/controller/HelloWorld.java
+++ src/main/java/com/metin/heroku/spring/boot/jar/deploy/herokuspringbootjardeploy/controller/HelloWorld.java
@@ -1,17 +1,50 @@
 package com.metin.heroku.spring.boot.jar.deploy.herokuspringbootjardeploy.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Metin on 18.05.2019.
  */
-
-@RestController
+@RestController()
+@RequestMapping("/api")
 public class HelloWorld {
 
-    @GetMapping("/")
-    public String sayHello(){
-        return "Hello World...";
+    @GetMapping("/v2/test")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void sayHello(){
+        System.out.printf("Selam Anıl");
     }
+
+    @PostMapping("/v2/test")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void sayHelloPost(){
+        System.out.printf("Selam Anıl");
+    }
+
+
+    @GetMapping("/v2/test1")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void sayHello(String token){
+        System.out.printf(token);
+    }
+
+    @PostMapping("/v2/test1")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void sayHelloPost(String token){
+        System.out.printf(token);
+    }
+
+    @GetMapping("/v2/test2")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void sayHello(Object object){
+        System.out.printf(object.toString());
+    }
+
+    @PostMapping("/v2/test2")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void sayHelloPost(Object object){
+        System.out.printf(object.toString());
+    }
+
 }
